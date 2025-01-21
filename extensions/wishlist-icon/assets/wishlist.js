31fc84fdaea861e4b7e57d4cf8e4e5363923bb18
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-21 22:14:56 +0000

    Toggle text

--- extensions/wishlist-icon/assets/wishlist.js
+++ extensions/wishlist-icon/assets/wishlist.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Get all wishlist buttons
-  const wishlistButtons = document.querySelectorAll('.wishlist-inspire__icon button');
+  const wishlistButtons = document.querySelectorAll(
+    ".wishlist-inspire__icon button",
+  );
 
   wishlistButtons.forEach((button) => {
-    button.addEventListener('click', function () {
+    button.addEventListener("click", function () {
       // Get the SVG inside the button
-      const svgIcon = button.querySelector('svg');
+      const svgIcon = button.querySelector("svg");
+      const currentTextElement = button.querySelector("span");
 
-      if (svgIcon) {
-        // Check the current color and toggle between two colors
-        const currentColor = svgIcon.getAttribute('fill');
-        const newColor = currentColor === 'none' ? "#ff0000" : 'none'; // Toggle between red and black
-        svgIcon.setAttribute('fill', newColor); // Update the color
-        //TODO: send data to database 
-      }
+      // Check the current color and toggle between two colors
+      const currentColor = svgIcon?.getAttribute("fill");
+      const currentText = currentTextElement?.textContent.trim();
+      const newColor = currentColor === "none" ? "#ff0000" : "none"; // Toggle between red and black
+      svgIcon.setAttribute("fill", newColor); // Update the color
+      const newText =
+        currentText === "Add to wishlist"
+          ? "Remove from wishlist"
+          : "Add to wishlist";
+      currentTextElement.textContent = newText;
+      //TODO: send data to database
     });
   });
 });
