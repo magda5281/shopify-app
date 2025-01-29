a47b89a1efef286dc3ecb072c03f3b7b13cdac50
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-29 12:33:57 +0000

    Update pricing page

--- app/routes/app.pricing.jsx
+++ app/routes/app.pricing.jsx
@@ -7,6 +7,9 @@ import {
   Page,
   Text,
   BlockStack,
+  Button,
+  Divider,
+  CalloutCard,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 
@@ -16,47 +19,61 @@ export default function PricingPage() {
       <TitleBar title="Pricing" />
       <Layout>
         <Layout.Section>
+          <CalloutCard
+            title=" Upgrade to pro"
+            illustration="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
+            primaryAction={{
+              content: " Upgrade to pro",
+              url: "#",
+            }}
+          >
+            <p>
+              {" "}
+              You are are currently on free plan. Upgrade to pro to unlock more
+              features.
+            </p>
+          </CalloutCard>
+        </Layout.Section>
+        <Layout.Section variant="oneHalf">
           <Card>
-            <BlockStack gap="300">
-              <Text as="p" variant="bodyMd">
-                The app template comes with an additional page which
-                demonstrates how to create multiple pages within app navigation
-                using{" "}
-                <Link
-                  url="https://shopify.dev/docs/apps/tools/app-bridge"
-                  target="_blank"
-                  removeUnderline
-                >
-                  App Bridge
-                </Link>
-                .
+            <BlockStack gap="200">
+              <Text as="h2" variant="headingSm">
+                Free
               </Text>
-              <Text as="p" variant="bodyMd">
-                To create your own page and have it show up in the app
-                navigation, add a page inside <Code>app/routes</Code>, and a
-                link to it in the <Code>&lt;NavMenu&gt;</Code> component found
-                in <Code>app/routes/app.jsx</Code>.
-              </Text>
+              <Text>Free plan with basic features</Text>
+              <Divider></Divider>
+              <List>
+                <List type="bullet">
+                  <List.Item>100 wishlist per day</List.Item>
+                  <List.Item>500 products</List.Item>
+                  <List.Item>Basic customization</List.Item>
+                  <List.Item>Basic support</List.Item>
+                </List>
+              </List>
+              <Divider></Divider>
+              <Button>Upgrade to pro</Button>
             </BlockStack>
           </Card>
         </Layout.Section>
-        <Layout.Section variant="oneThird">
+        <Layout.Section variant="oneHalf">
           <Card>
             <BlockStack gap="200">
               <Text as="h2" variant="headingMd">
-                Resources
+                Pro
               </Text>
+              <Text>Pro plan with advanced features</Text>
+              <Text variant="heading2xl" as="p" fontWeight="bold">
+                $ 10
+              </Text>
+              <Divider></Divider>
               <List>
-                <List.Item>
-                  <Link
-                    url="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-                    target="_blank"
-                    removeUnderline
-                  >
-                    App nav best practices
-                  </Link>
-                </List.Item>
+                <List.Item>Unlimited wishlist per day</List.Item>
+                <List.Item>100000 products</List.Item>
+                <List.Item>Advanced customization</List.Item>
+                <List.Item>Priority support</List.Item>
               </List>
+              <Divider></Divider>
+              <Button>Upgrade to pro</Button>
             </BlockStack>
           </Card>
         </Layout.Section>
